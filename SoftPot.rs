502a684048297305e929ab59496226919c731ff5
Erich Gubler <erichdongubler@gmail.com>
2020-04-23 12:14:04 -0600

    style: run `cargo fmt`, add `rustfmt::skip` annotations where seemingly intended

--- SoftPot.rs
+++ SoftPot.rs
@@ -1,14 +1,14 @@
+use sample::{signal, Signal};
 use serial::prelude::*;
 use serial::ErrorKind::NoDevice;
-use sample::{signal, Signal};
 
 use std::io::Read;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::sync::{Mutex, Arc};
 
 // use rand::Rng;
-use cpal::{StreamData, UnknownTypeOutputBuffer};
 use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+use cpal::{StreamData, UnknownTypeOutputBuffer};
 
 fn main() {
     // Set up port to get data from arduino
@@ -18,37 +18,49 @@ fn main() {
             if error.kind() == NoDevice {
                 println!("No device was found!");
             }
-            return
-        },
+            return;
+        }
     };
-    port.reconfigure(&|settings|{
-        settings.set_baud_rate(serial::Baud9600)
-    }).unwrap();
+    port.reconfigure(&|settings| settings.set_baud_rate(serial::Baud9600))
+        .unwrap();
 
     // Set up audio stuff
     let host = cpal::default_host();
     let event_loop = host.event_loop();
-    let device = host.default_output_device().expect("no output device available");
-    let mut supported_formats_range = device.supported_output_formats()
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+    let mut supported_formats_range = device
+        .supported_output_formats()
         .expect("error while querying formats");
-    let format = supported_formats_range.next()
+    let format = supported_formats_range
+        .next()
         .expect("no supported format?!")
-        .with_max_sample_rate();    
+        .with_max_sample_rate();
     let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
-    event_loop.play_stream(stream_id).expect("failed to play_stream");
+    event_loop
+        .play_stream(stream_id)
+        .expect("failed to play_stream");
 
     // Define each note
+    #[rustfmt::skip]
     let mut C      = signal::rate(44_100.0).const_hz(261.626).sine();
     let mut Csharp = signal::rate(44_100.0).const_hz(277.183).sine();
+    #[rustfmt::skip]
     let mut D      = signal::rate(44_100.0).const_hz(293.665).sine();
     let mut Dsharp = signal::rate(44_100.0).const_hz(311.127).sine();
+    #[rustfmt::skip]
     let mut E      = signal::rate(44_100.0).const_hz(329.628).sine();
+    #[rustfmt::skip]
     let mut F      = signal::rate(44_100.0).const_hz(349.228).sine();
     let mut Fsharp = signal::rate(44_100.0).const_hz(369.994).sine();
+    #[rustfmt::skip]
     let mut G      = signal::rate(44_100.0).const_hz(391.995).sine();
     let mut Gsharp = signal::rate(44_100.0).const_hz(415.305).sine();
+    #[rustfmt::skip]
     let mut A      = signal::rate(44_100.0).const_hz(440.000).sine();
     let mut Asharp = signal::rate(44_100.0).const_hz(466.164).sine();
+    #[rustfmt::skip]
     let mut B      = signal::rate(44_100.0).const_hz(493.883).sine();
 
     let current_note = Arc::new(Mutex::new(0));
@@ -56,19 +68,19 @@ fn main() {
     let play_note = Arc::clone(&current_note);
     thread::spawn(move || {
         loop {
-            let mut slice = [0;2];
+            let mut slice = [0; 2];
             let resultythingy = port.read_exact(&mut slice[..]);
             match resultythingy {
                 Ok(()) => {
                     // println!("                                      Everything is ok!");
                     // The arduino sends ascii, and 0 in ascii is 48.
-                    let read_note = (i32::from(slice[0]%48)* 10) + i32::from(slice[1]%48);
+                    let read_note = (i32::from(slice[0] % 48) * 10) + i32::from(slice[1] % 48);
                     let mut note = get_note.lock().unwrap();
                     if read_note != *note {
                         *note = read_note
                     }
                     // println!("                                          Note! {:?}", *note)
-                },
+                }
                 Err(resultythingy) => {
                     // println!("                                      Oh no! {:?}", resultythingy);
                 }
@@ -77,38 +89,49 @@ fn main() {
     });
     event_loop.run(move |_stream_id, _stream_result| {
         // This unlocks the mutex before the loop so the other thread can use it faster.
-        let note = {
-            *play_note.lock().unwrap()
-        };
+        let note = { *play_note.lock().unwrap() };
         println!("reading in event note: {}", note);
         let stream_data = _stream_result.unwrap();
         match stream_data {
-            StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
+            StreamData::Output {
+                buffer: UnknownTypeOutputBuffer::F32(mut buffer),
+            } => {
                 // I was fiddling with static as a test before I figured out specific notes.
                 // let mut rng = rand::thread_rng();
                 for elem in buffer.iter_mut() {
                     // *elem = rng.gen::<f32>()
                     let next_value;
                     match note {
+                        #[rustfmt::skip]
                         0  => next_value = 0.0,
+                        #[rustfmt::skip]
                         1  => next_value = C.next()[0],
+                        #[rustfmt::skip]
                         2  => next_value = Csharp.next()[0],
+                        #[rustfmt::skip]
                         3  => next_value = D.next()[0],
+                        #[rustfmt::skip]
                         4  => next_value = Dsharp.next()[0],
+                        #[rustfmt::skip]
                         5  => next_value = E.next()[0],
+                        #[rustfmt::skip]
                         6  => next_value = F.next()[0],
+                        #[rustfmt::skip]
                         7  => next_value = Fsharp.next()[0],
+                        #[rustfmt::skip]
                         8  => next_value = G.next()[0],
+                        #[rustfmt::skip]
                         9  => next_value = Gsharp.next()[0],
                         10 => next_value = A.next()[0],
                         11 => next_value = Asharp.next()[0],
                         12 => next_value = B.next()[0],
-                        _  => next_value = 0.0
+                        #[rustfmt::skip]
+                        _  => next_value = 0.0,
                     }
-                    *elem = (next_value/5.0) as f32;
+                    *elem = (next_value / 5.0) as f32;
                 }
-            },
-            _ => ()
+            }
+            _ => (),
         }
     });
 }
